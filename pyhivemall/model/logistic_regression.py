e2bef2ea79691bb7a28de4d636672292cb0a617a
Takuya Kitazawa <k.takuti@gmail.com>
2018-02-01 06:01:55 +0900

    Load coefficients besides creating vectorizer

--- pyhivemall/model/logistic_regression.py
+++ pyhivemall/model/logistic_regression.py
@@ -1,4 +1,5 @@
 from sklearn import linear_model
+from sklearn.feature_extraction import DictVectorizer
 import numpy as np
 
 
@@ -7,15 +8,31 @@ class LogisticRegression(linear_model.LogisticRegression):
     def __init__(self, source_dataframe=None, feature_column='feature', weight_column='weight', bias_feature=None, **kwargs):
         super(linear_model.LogisticRegression, self).__init__(**kwargs)
 
-        intercept_ = np.array([0])
-        coef_ = np.array([])
+        intercept = np.array([0.])  # (1,)
+        coef = np.array([[]])  # (1, n_feature)
+
+        vocabulary = {}
+        feature_names = []
 
         if source_dataframe is not None:
             for i, row in source_dataframe.iterrows():
-                if row[feature_column] == bias_feature:
-                    intercept_[0] = float(row[weight_column])
-                    continue
-                # TODO: update coef_ here
+                feature, weight = row[feature_column], row[weight_column]
 
-        self.intercept_ = intercept_
-        self.coef_ = coef_
+                if feature == bias_feature:
+                    intercept[0] = float(weight)
+                    continue
+
+                coef = np.append(coef, [[weight]], axis=1)
+
+                vocabulary[feature] = i
+                feature_names.append(feature)
+
+        self.vectorizer_ = DictVectorizer(separator='#')
+        self.vectorizer_.vocabulary_ = vocabulary
+        self.vectorizer_.feature_names_ = feature_names
+
+        self.intercept_ = intercept
+        self.coef_ = coef
+
+    def vectorize(self, X):
+        return self.vectorizer_.transform(X)
