497bb2a09f09378b849ec26a953d2bdc885a9f8a
Takuya Kitazawa <k.takuti@gmail.com>
2018-02-02 07:23:17 +0900

    Add Hadoop-related configs to make HiveServer2 connectable

--- docker/README.md
+++ docker/README.md
@@ -1,6 +1,14 @@
 ## Running on Docker
 
+> Note: This Docker container is experimental and may not work correctly.
+
 ```
 $ docker build -t pyhivemall .
-$ docker run --rm -it -p 10000:10000 pyhivemall sh -c "./bin/init.sh && hive --service hiveserver2"
+$ docker run --rm -it -p 10000:10000 pyhivemall
+```
+
+```py
+from pyhivemall import HiveConnection
+
+conn = HiveConnection(host='localhost', port=10000)
 ```
---
+++ docker/init.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -eux
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+/etc/init.d/ssh start
+$HADOOP_HOME/sbin/start-dfs.sh
+$HADOOP_HOME/sbin/start-yarn.sh
+$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh start historyserver
+[ -d ~/metastore_db ] || schematool -initSchema -dbType derby
+
+hdfs dfs -mkdir /tmp/hive
+hdfs dfs -chmod 775 /tmp/hive
+
+hive --service hiveserver2
