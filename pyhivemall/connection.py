fb074d1be165d27b72ff25aed3c7939b43f7a9e4
Takuya Kitazawa <k.takuti@gmail.com>
2018-02-09 12:47:51 +0900

    Fix wrong query issued by HiveConnection to fetch table

--- pyhivemall/connection.py
+++ pyhivemall/connection.py
@@ -18,9 +18,7 @@ class HiveConnection(BaseConnection):
         self.conn = hive.Connection(host=host, port=port, database=database)
 
     def fetch_table(self, table, columns=['*']):
-        return pd.read_sql('select %(columns)s from %(table)s',
-                           self.conn,
-                           params={'columns': ', '.join(columns), 'table': table})
+        return pd.read_sql('select %s from %s' % (', '.join(columns), table), self.conn)
 
 
 class TdConnection(BaseConnection):
--- pyhivemall/model/__init__.py
+++ pyhivemall/model/__init__.py
@@ -2,7 +2,7 @@ from .logistic_regression import LogisticRegression
 
 
 def load_model(model, table, conn, **kwargs):
-    df = conn.fetch_table(table)
+    df = conn.fetch_table(table, columns=[kwargs['feature_column'], kwargs['weight_column']])
     if model == 'LogisticRegression':
         return LogisticRegression.load(source_dataframe=df, **kwargs)
     raise ValueError("model should be 'LogisticRegression', got %s" % model)
